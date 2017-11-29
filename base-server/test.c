9ea25783aba3b4442f864534b2f5024fe8063df2
Bruno Mondelo <mondelob14@gmail.com>
2017-11-29 16:54:34 +0100

    Testing loggetr

--- base-server/test.c
+++ base-server/test.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
-#include "konet.h"
+#include <unistd.h>
+#include "logger.h"
 
 int
 main(void) {
+  const char * err, * msg;
+  struct log * l;
+
+  // l = logger_descriptor(STDOUT_FILENO, &err);
+  l = logger_filename("/dev/stdout", &err);
+  if (l == NULL) {
+    printf("Error creating logger\n");
+    printf("Error trace : %s\n", err);
+  }
+
+  msg = "Hello World!";
+  logger_write(l, "Hello World!\n", &err);
+  logger_write(l, "abbcccddddeeeeeffffffg\n", &err);
 
   return 0;
 }
